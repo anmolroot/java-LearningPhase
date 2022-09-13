dff87bf5be7f8b1c2e07fec51f617212e36d70fb
ANMOL SAROHA <51941110+anmolroot@users.noreply.github.com>
2022-09-13 22:12:26 +0530

    Add files via upload

---
+++ DataTypes.java
@@ -0,0 +1,13 @@
+package JavaBasics;
+
+public class DataTypes {
+    public static void main(String[] arg) {
+        byte x = 126;
+        float rateOfInterest = 2.3f;
+        double rate = 3.1428571;
+        boolean isThisSeriesCool = true;
+        char myCharacter = '@';
+        System.out.println(x);
+        System.out.println(myCharacter);
+    }
+}
